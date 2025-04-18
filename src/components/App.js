2d2f3a1f829f57fb40a27a49a5a2d9132286495b
elizabeth ndinda <elizabethndinda41@gmail.com>
2025-04-18 09:18:26 +0300

    transforming a list of values into a list of components

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,24 @@
+
 import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 import ProjectList from "./ProjectList";
-import user from "../data/user";
+import user from "../data/user";  
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
+      <Home />
+      <About />
       <ProjectList projects={user.projects} />
+      {/* Display user info if needed */}
+      <div style={{ color: user.color }}>
+        <h2>{user.name}</h2>
+        <p>{user.city}</p>
+        <p>{user.bio}</p>
+      </div>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,21 @@
 import React from "react";
 
 function NavBar() {
-  const links = ["home", "about", "projects"];
+  const Links = [
+    { id: 1, href: "#home", Label: "Home" },
+    { id: 2, href: "#about", Label: "About" },
+    { id: 3, href: "#projects", Label: "Projects" },
+  ];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {Links.map((link) => (
+        <a key={link.id} href={link.href}>
+          {link.Label}
+        </a>
+      ))}
+    </nav>
+  );
 }
 
 export default NavBar;
